f113fcfa255ef890b05c784b5cd0357372eb2ce1
Ethan Marantz <ebmarantz@gmail.com>
2026-05-04 00:50:56 -0400

    added ability to open images using oil.nvim

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -6,6 +6,26 @@ return {
     view_options = {
       show_hidden = false,
     },
+    keymaps = {
+      ['<CR>'] = {
+        desc = 'Open file (images go to imv)',
+        callback = function()
+          local oil = require 'oil'
+          local entry = oil.get_cursor_entry()
+          local dir = oil.get_current_dir()
+          if not entry or not dir then return end
+
+          local image_exts = { png = true, jpg = true, jpeg = true, gif = true, webp = true, bmp = true, avif = true, jxl = true }
+          local ext = entry.name:match '%.([^.]+)$'
+
+          if ext and image_exts[ext:lower()] then
+            vim.fn.jobstart({ 'imv', dir .. entry.name }, { detach = true })
+          else
+            oil.select()
+          end
+        end,
+      },
+    },
   },
   -- Optional dependencies
   dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
