87faa057074a68c7e4f2ead2361d49d597702ae9
ethan <ethan@debian-server.home.arpa>
2026-05-24 12:56:41 -0400

    added copy and paste support in nvim for servers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -93,6 +93,20 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
+if vim.env.DISPLAY == nil and vim.env.WAYLAND_DISPLAY == nil then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
+end
+
 -- [[ Setting options ]]
 require 'options'
 
