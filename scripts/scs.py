0c3d1f3b620f064e300292ebf5537c10744caf33
Ethan Marantz <ebmarantz@gmail.com>
2026-06-01 06:04:56 -0400

    added imv preview optimization

--- scripts/scs.py
+++ scripts/scs.py
@@ -24,10 +24,22 @@ for uid, date, title, ocr in rows:
 conn.commit()
 conn.close()
 
+if not lines:
+    print("No screenshots found.")
+    sys.exit()
+
 input_text = "\n".join(lines)
+
+first_uid = lines[0].split("\t")[0]
+imv_proc = subprocess.Popen(["imv", str(Path.home() / "Pictures" / f"{first_uid}.png")])
+imv_pid = imv_proc.pid
+
 subprocess.run(
-    ["fzf", "--delimiter=\t", "--bind=enter:execute(imv ~/Pictures/{1}.png)",
-     f"--bind=ctrl-y:execute(sqlite3 {db_path} \"SELECT ocr FROM screenshots WHERE uid={{1}}\" | wl-copy)"],
+    ["fzf", "--delimiter=\t",
+     f"--bind=focus:execute-silent(imv-msg {imv_pid} open ~/Pictures/{{1}}.png && imv-msg {imv_pid} goto -1)",
+     f"--bind=enter:execute(sqlite3 {db_path} \"SELECT ocr FROM screenshots WHERE uid={{1}}\" | wl-copy)"],
     input=input_text,
     text=True
 )
+
+imv_proc.terminate()
