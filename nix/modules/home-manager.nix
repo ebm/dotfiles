14368bfa5dc933ddb6716c754d4c405a56667d88
Ethan Marantz <ebmarantz@gmail.com>
2026-06-13 07:47:39 -0400

    deleted deprecated home-manager.nix from modules

--- nix/modules/home-manager.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, ... }: {
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = {
-    inherit inputs;
-  };
-  home-manager.users.ethan = ../home.nix;
-}
