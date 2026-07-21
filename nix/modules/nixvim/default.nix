6ce6b0413ec72ff2e26476a330ec89b32eec9d20
Ethan Marantz <ebmarantz@gmail.com>
2026-07-21 11:28:48 +0200

    fixed flake

--- nix/modules/nixvim/default.nix
+++ nix/modules/nixvim/default.nix
@@ -9,7 +9,7 @@
 
   programs.nixvim = {
     enable = true;
-    useGlobalPackages = true;
+    nixpkgs.useGlobalPackages = true;
     defaultEditor = true;
     globals.mapleader = " ";
     colorschemes.catppuccin.enable = true;
