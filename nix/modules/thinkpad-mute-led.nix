218afe586151ae7670898519d9d17a2648220d10
Ethan Marantz <ebmarantz@gmail.com>
2026-08-02 20:57:59 -0400

    adjusted comments

--- nix/modules/thinkpad-mute-led.nix
+++ nix/modules/thinkpad-mute-led.nix
@@ -1,10 +1,7 @@
 { ... }:
 
-# The ThinkPad mute (F1) and mic-mute (F4) LEDs are bound to kernel LED
-# triggers ("audio-mute" / "audio-micmute") that track hardware mute switches.
-# We don't want either indicator light, so detach both from their triggers and
-# blank them -- muting still works, the lights just stay off.
 {
+  # Removing volume and mic mute LED
   services.udev.extraRules = ''
     SUBSYSTEM=="leds", KERNEL=="platform::micmute", ACTION=="add", ATTR{trigger}="none", ATTR{brightness}="0"
     SUBSYSTEM=="leds", KERNEL=="platform::mute", ACTION=="add", ATTR{trigger}="none", ATTR{brightness}="0"
