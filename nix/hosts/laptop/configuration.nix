5c11c104fe980d851b2e0a823f101d68dbdbcf09
Ethan Marantz <ebmarantz@gmail.com>
2026-08-28 18:02:50 -0400

    added laptop sleeping

--- nix/hosts/laptop/configuration.nix
+++ nix/hosts/laptop/configuration.nix
@@ -9,6 +9,9 @@
     ./disko-config.nix
   ];
 
+  # Closing the lid suspends, then hibernates after HibernateDelaySec in RAM.
+  services.logind.settings.Login.HandleLidSwitch = "suspend-then-hibernate";
+
   programs.captive-browser = {
     enable = true;
     interface = "wlp1s0";
