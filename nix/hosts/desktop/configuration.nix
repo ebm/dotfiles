44f715e0e9b136cf7f903a45b97bd3904966c80e
Ethan Marantz <ebmarantz@gmail.com>
2026-09-04 20:45:39 -0400

    added efibootgr to systemPackages

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -15,6 +15,9 @@
   # Lofree Flow84 uses the hid_apple driver; fnmode=2 makes F1-F12 primary
   boot.kernelParams = [ "hid_apple.fnmode=2" ];
 
+  # Windows dual-boots from a separate SSD; efibootmgr manages the EFI BootOrder
+  environment.systemPackages = [ pkgs.efibootmgr ];
+
   programs.steam.enable = true;
 
   time.hardwareClockInLocalTime = true;
